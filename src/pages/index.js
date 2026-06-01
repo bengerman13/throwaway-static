96c76569e07ecf653454e94d769e9039cc55cfc9
Ben Berry <ben@benberry.dev>
2026-06-01 13:00:50 -0700

    make it usable

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ export default function Home() {
   return (
     <div>
       Hello World.{" "}
-      <Link href="/about">
-        About
-      </Link>
-      <Link href="/app/">
-        App
-      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/app/">Marketplace</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
